f5c7f5cdd2428f33811a509da03ec16f70087363
Johan Larsson <johan_gavle1@hotmail.com>
2017-05-25 12:27:37 +0200

    Matching template, drawing box.

--- OpenCVSharpBox/MatchTemplateView.xaml.cs
+++ OpenCVSharpBox/MatchTemplateView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿namespace OpenCVSharpBox
 {
+    using System.Linq;
     using System.Windows.Controls;
     using OpenCvSharp;
     using OpenCvSharp.Extensions;
@@ -10,9 +11,60 @@
         public MatchTemplateView()
         {
             this.InitializeComponent();
+            //this.FindAndDrawMatches();
+            this.FindAndDrawHomo();
+        }
+
+        private void FindAndDrawHomo()
+        {
             using (var template = new Mat("Images\\Circle_Template.bmp", ImreadModes.GrayScale))
             {
-                using (var surf = SURF.Create(200))
+                using (var surf = SURF.Create(1000))
+                {
+                    using (var templateDescriptors = new Mat())
+                    {
+                        surf.DetectAndCompute(template, null, out KeyPoint[] templateKeyPoints, templateDescriptors);
+                        using (var image = new Mat("Images\\Circle.bmp", ImreadModes.GrayScale))
+                        {
+                            using (var imageDescriptors = new Mat())
+                            {
+                                surf.DetectAndCompute(image, null, out KeyPoint[] imageKeyPoints, imageDescriptors);
+                                using (var matcher = new BFMatcher())
+                                {
+                                    var matches = matcher.Match(imageDescriptors, templateDescriptors);
+                                    var goodMatches = matches.Where(m => m.Distance < 0.2).ToArray();
+                                    using (var srcPoints = InputArray.Create(goodMatches.Select(m => templateKeyPoints[m.TrainIdx].Pt)))
+                                    {
+                                        using (var dstPoints = InputArray.Create(goodMatches.Select(m => imageKeyPoints[m.QueryIdx].Pt)))
+                                        {
+                                            using (var homo = Cv2.FindHomography(srcPoints, dstPoints, HomographyMethods.Ransac))
+                                            {
+                                                using (var overLay = image.OverLay())
+                                                {
+                                                    var corners = Cv2.PerspectiveTransform(template.Corners(), homo);
+                                                    Cv2.Line(overLay, corners[0], corners[1], Scalar4.Red);
+                                                    Cv2.Line(overLay, corners[1], corners[2], Scalar4.Red);
+                                                    Cv2.Line(overLay, corners[2], corners[3], Scalar4.Red);
+                                                    Cv2.Line(overLay, corners[3], corners[0], Scalar4.Red);
+                                                    //Cv2.WarpPerspective(template, overLay, homo, overLay.Size());
+                                                    this.Result.Source = overLay.ToBitmapSource();
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void FindAndDrawMatches()
+        {
+            using (var template = new Mat("Images\\Circle_Template.bmp", ImreadModes.GrayScale))
+            {
+                using (var surf = SURF.Create(1000))
                 {
                     using (var templateDescriptors = new Mat())
                     {
